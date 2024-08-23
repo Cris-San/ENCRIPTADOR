952d805da83c1fc4dffc7bfb79be51cec45047ae
Cris-San <cris629.cso@gmail.com>
2024-08-23 00:36:06 -0500

    Se copia la funcionalidad de encriptar para desencriptar.Se crea el selector para btncopiar y para btndesencriptar

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const txtEncriptar = document.querySelector(".txtareaingreso");
 const aviso = document.querySelector(".texto-aviso");
 const txtDesencriptar = document.querySelector(".txtareaSalida");
 const contenido = document.querySelector(".tarjeta-contenedor");
+const copiar = document.querySelector(".btn_copiar");
+const botonDesencriptar = document.querySelector(".btn_desencrip");
 
 //---///Boton de Encriptar Eventos///
 
@@ -55,9 +57,52 @@ botonEncriptar.addEventListener("click", e => {
 
         txtDesencriptar.innerHTML = texto;
         contenido.remove();
+    }    
+});
 
+//---///Boton de Desencriptar Eventos///
+botonDesencriptar.addEventListener("click", e => {
+    e.preventDefault();
+    let texto = txtEncriptar.value;
+    let txt = texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, ""); 
 
+    if (texto === "") {
+        aviso.style.background = "#0A387";
+        aviso.style.color = "#FFF";
+        aviso.style.fontWeight = "1200";
+        aviso.textContent = "El campo de texto no puede estar vacio";
+        setTimeout(() => {
+         aviso.removeAttribute("style");
+        }, 1500);
+    }
+    else if (texto !== txt) {
+        aviso.style.background = "#0A387";
+        aviso.style.color = "#FFF";
+        aviso.style.fontWeight = "1200";
+        aviso.textContent = "El texto no debe tener acentos ni caracteres especiales";
+        
+        setTimeout(() => {
+            aviso.removeAttribute("style");
+        }, 1500);   
+    }
+    else if (texto !== texto.toLowerCase()) {
+        aviso.style.background = "#0A387";
+        aviso.style.color = "#FFF";
+        aviso.style.fontWeight = "1200";
+        aviso.textContent = "El texto debe ser todo en minúscula";
+        
+        setTimeout(() => {
+            aviso.removeAttribute("style");
+        }, 1500);
+    }
+    else {
+        texto = texto.replace(/enter/mg, "e");
+        texto = texto.replace(/imes/mg, "i");
+        texto = texto.replace(/ai/mg, "a");
+        texto = texto.replace(/ober/mg, "o");
+        texto = texto.replace(/ufat/mg, "u");
+        txtDesencriptar.innerHTML = texto;
+        contenido.remove();
     }    
 
-    
-})
+});
